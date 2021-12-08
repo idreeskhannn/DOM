9c552881df768c54ad95f51899301f7cb3ee8b3d
muhamaad idrees <midreeskhan7276@gmail.com>
2021-12-08 10:31:43 -0800

    practice

---
+++ app.js
@@ -0,0 +1,75 @@
+// alert("hello world")
+
+// chapter # 58 to 67
+
+
+//  part = 1
+
+// var mainContent = document.getElementById("main-content")
+// var p1 = document.createElement("p")
+// var p2 = document.createElement("p")
+// var p3 = document.createElement("p")
+// var p4 = document.createElement("p")
+// var p5 = document.createElement("p")
+
+// var txt1 = document.createTextNode("First Name : Alex")
+// var txt2 = document.createTextNode("last  Name : Bank")
+// var txt3 = document.createTextNode("Email : AlexBank@gmail.com")
+// var txt4 = document.createTextNode("Country : Pakistan")
+// var txt5 = document.createTextNode("Contact  : 0123456788")
+
+// p1.appendChild(txt1)
+// p2.appendChild(txt2)
+// p3.appendChild(txt3)
+// p4.appendChild(txt4)
+// p5.appendChild(txt5)
+
+// mainContent.appendChild(p1)
+// mainContent.appendChild(p2)
+// mainContent.appendChild(p3)
+// mainContent.appendChild(p4)
+// mainContent.appendChild(p5)
+
+
+
+
+// pasrt = 2
+
+// var formContent = document.getElementById("form-content")
+ // console.log(formContent.nodeType)
+// console.log(formContent)
+
+// var last = document.getElementById("last-name")
+// // console.log(last.nodeName)
+// // console.log(last.nodeType)
+// console.log(last.children)
+
+
+// var mainForm = document.getElementById("main-content")
+// // console.log(mainForm.children)
+// var id=document.getElementById("lastName")
+// var txt = document.createTextNode("khan")
+// id.appendChild(txt)
+// mainForm.appendChild(id)
+
+
+// var id=document.getElementById("last-name")
+// console.log(id.nextElementSibling)
+
+
+
+// var id=document.getElementById("last-name")
+// console.log(id.previousElementSibling)
+
+
+
+// var email = document.getElementById("email")
+// console.log(email.parentNode)
+
+
+
+// var email = document.getElementById("email")
+// console.log(email.nodeType)
+
+// var email = document.getElementById("email")
+// console.log(email.nodeName)
